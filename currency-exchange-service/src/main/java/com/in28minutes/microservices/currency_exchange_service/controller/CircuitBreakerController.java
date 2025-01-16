906c37b537f0a7ffa64913ea0ee5c4142546fc6b
shivam <shivamd@farmkart.com>
2025-01-16 11:53:52 +0530

    spring cloud done!

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/controller/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.microservices.currency_exchange_service.controller;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CircuitBreakerController {
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api-retry", fallbackMethod = "hardCodedMethod")
     // if error comes, it will call the same url total 5 times
+    @Retry(name = "sample-api-retry", fallbackMethod = "hardCodedMethod")
+
+    // For rate limiting (At max 2 calls, in 10 sec)
+    @RateLimiter(name = "default")
     public String sampleApi() {
         System.out.println("sample-api Called!");
         return new RestTemplate().getForEntity(
